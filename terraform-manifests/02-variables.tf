ce5e0be195750d0c273d41107eda1bbca00ffaf6
Young Bae <young.bae@hpe.com>
2021-12-23 16:23:09 -0800

    changed location to west us 2

--- terraform-manifests/02-variables.tf
+++ terraform-manifests/02-variables.tf
@@ -7,7 +7,7 @@
 variable "location" {
   type = string
   description = "Azure Region where all these resources will be provisioned"
-  default = "Central US"
+  default = "West US 2"
 }
 
 # Azure Resource Group Name
